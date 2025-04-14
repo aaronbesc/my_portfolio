2dee78b5b06fb1cf0745abcba8ce8197b22b50e4
Aaron Beschorner Lira <aaronbeschorner@gmail.com>
2025-04-14 01:21:19 -0400

    Some additional fixes for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,38 +16,6 @@ type Project = {
 
 export default function Home() {
   const [projects, setProjects] = useState<Project[]>([]);
-  const [displayText, setDisplayText] = useState(""); // State for typewriter effect
-  const words = ["Computer Science major", "Electrical Engineering minor", "senior UF student"];
-
-  useEffect(() => {
-    let i = 0;
-    let j = 0;
-    let isDeleting = false;
-
-    const typeEffect = () => {
-      const currentWord = words[i];
-
-      if (isDeleting) {
-        setDisplayText((prev) => prev.slice(0, -1));
-        j--;
-        if (j === 0) {
-          isDeleting = false;
-          i = (i + 1) % words.length;
-        }
-      } else {
-        setDisplayText((prev) => currentWord.slice(0, j + 1));
-        j++;
-        if (j === currentWord.length) {
-          isDeleting = true;
-          setTimeout(typeEffect, 1500); // Delay before deleting
-          return;
-        }
-      }
-      setTimeout(typeEffect, isDeleting ? 100 : 50);
-    };
-
-    typeEffect();
-  }, []);
 
   useEffect(() => {
     fetch("/data/projects.json")
@@ -59,7 +27,13 @@ export default function Home() {
     <>
       {/* Full-width Wave Background */}
       <div className="bg-zinc-900">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 175"><path fill="#fff" fill-opacity="1" d="M0,96L60,80C120,64,240,32,360,48C480,64,600,128,720,138.7C840,149,960,107,1080,85.3C1200,64,1320,64,1380,64L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 175">
+          <path
+            fill="#fff"
+            fillOpacity="1"
+            d="M0,96L60,80C120,64,240,32,360,48C480,64,600,128,720,138.7C840,149,960,107,1080,85.3C1200,64,1320,64,1380,64L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          ></path>
+        </svg>
       </div>
 
       <div className="container mx-auto p-6">
@@ -69,7 +43,7 @@ export default function Home() {
             My name is Aaron Beschorner
           </p>
           <p className="text-zinc-900 libre-baskerville-regular text-5xl mt-3 tracking-tighters">
-            and I'm an  <span className="libre-baskerville-regular">aspiring SWE</span>.
+            and I&apos;m an <span className="libre-baskerville-regular">aspiring SWE</span>.
           </p>
           <div className="flex mt-10 space-x-1 justify-center">
             <SocialButtons />
@@ -78,10 +52,9 @@ export default function Home() {
             After countless —and completely unnecessary— hours on Pinterest, battling my toughest critic (me, myself, and I) over which font to use, 
             I am thrilled to announce the launch of the very first version (v0.1.0) of my personal website! YAY! *crickets*.
             Here you will find my coding projects, blog posts, and socials. I am currently looking for internship opportunities for Summer 2025 or Fall 2025. 
-              Feel free to connect or shoot me a message!
+            Feel free to connect or shoot me a message!
           </p>
         </header>
-
 
         {/* Projects Section */}
         <section>
@@ -90,7 +63,23 @@ export default function Home() {
             {projects.map((project, index) => (
               <div key={project.id} className="border border-black p-3 flex items-center justify-between">
                 .0{index + 1}
-                <a href={project.githubLink}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg></a>
+                <a href={project.githubLink}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-github"
+                  >
+                    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                    <path d="M9 18c-4.51 2-5-2-7-2" />
+                  </svg>
+                </a>
               </div>
             ))}
 
@@ -128,32 +117,43 @@ export default function Home() {
 
         {/* Contact Section */}
         {/* Footer */}
-        </div>
+      </div>
 
-        <section className="text-center mt-10 p-6 border rounded-lg shadow-md">
-          <h2 className="text-3xl font-bold">Do you want to work with me?</h2>
-          <p className="text-gray-600 mt-2">Feel free to reach out for collaboration or project inquiries.</p>
-          <Button className="mt-4" asChild>
-            <a href="mailto:aaronbeschorner@gmail.com">Contact Me</a>
-          </Button>
-        </section>
+      <section className="text-center mt-10 p-6 border rounded-lg shadow-md">
+        <h2 className="text-3xl font-bold">Do you want to work with me?</h2>
+        <p className="text-gray-600 mt-2">Feel free to reach out for collaboration or project inquiries.</p>
+        <Button className="mt-4" asChild>
+          <a href="mailto:aaronbeschorner@gmail.com">Contact Me</a>
+        </Button>
+      </section>
 
-        <div className="bg-zinc-900">
+      <div className="bg-zinc-900">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150">
-          <path fill="#fff" fill-opacity="1" d="M0,96L48,101.3C96,107,192,117,288,128C384,139,480,149,576,149.3C672,149,768,139,864,122.7C960,107,1056,85,1152,74.7C1248,64,1344,64,1392,64L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+          <path
+            fill="#fff"
+            fillOpacity="1"
+            d="M0,96L48,101.3C96,107,192,117,288,128C384,139,480,149,576,149.3C672,149,768,139,864,122.7C960,107,1056,85,1152,74.7C1248,64,1344,64,1392,64L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
         </svg>
-        </div>
+      </div>
 
-        <footer className="bg-zinc-900 text-center text-white p-6 flex items-center justify-center space-x-2">
-          <span>with</span>
-          <span className="text-red-500">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className="inline-block">
-              <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
-            </svg>
-          </span>
-          <span>from</span>
-          <span className="text-white">Aaron Beschorner</span>
-        </footer>
+      <footer className="bg-zinc-900 text-center text-white p-6 flex items-center justify-center space-x-2">
+        <span>with</span>
+        <span className="text-red-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="inline-block"
+          >
+            <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
+          </svg>
+        </span>
+        <span>from</span>
+        <span className="text-white">Aaron Beschorner</span>
+      </footer>
     </>
   );
 }
